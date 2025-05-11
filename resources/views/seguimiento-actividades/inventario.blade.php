64207957107d291833e15e4a86f864232f74d978
Cesar <calfonsomora@gmail.com>
2025-05-10 20:51:44 -0500

    Actualización minima.

--- resources/views/seguimiento-actividades/inventario.blade.php
+++ resources/views/seguimiento-actividades/inventario.blade.php
@@ -227,6 +227,7 @@
 @endsection
 
 @push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             //cargarInventario();
